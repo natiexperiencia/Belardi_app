d0cb2ea3985cc01bd1ed769ce2b63358c4248ca1
natiexperiencia <natiexperiencia@gmail.com>
2015-05-27 09:50:24 +0200

    acabado consulta usuarios y usuario

--- Belardi/php/consultaUsuarios.php
+++ Belardi/php/consultaUsuarios.php
@@ -3,11 +3,17 @@
 	$tipo = $_POST['radioUsuario'];
 	include('conectar.php');
 	$link = Conectarse();
-	//$arrayJason = [];
+	$arrayJson = array();
+	$nombre = $_POST['nombreUsuario'];
 	
 	if ($tipo == "todos") {
 		$usuarios = mysql_query('select * from usuarios', $link);
 		while ($row = mysql_fetch_array($usuarios)) {
+			array_push($arrayJson, array('numero' => $row[0], 'nombre' => $row[1], 'email' => $row[2], 'rol' => $row[4]));
+		}
+	}else{
+		$usuario = mysql_query('select * from usuarios where nombre = "'.$nombre.'"', $link);
+		while ($row = mysql_fetch_array($usuario)) {
 			$arrayJson = array('numero' => $row[0], 'nombre' => $row[1], 'email' => $row[2], 'rol' => $row[4]);
 		}
 	}
--- Belardi/www/js/consultar.js
+++ Belardi/www/js/consultar.js
@@ -1,28 +1,61 @@
 $(document).ready(function() {
 	$("#formularioConsulta").submit(function(event) {
+
 		console.log("submit");
 		event.preventDefault();
 		var tipoConsulta = $("input[name=radio]:checked", "#formularioConsulta").val();
-		if(tipoConsulta == "usuarios"){
-			var formData = $(this).serializeArray();
+		var tipoConsultaUsuario = $("input[name=radioUsuario]:checked", "#formularioConsulta").val();
 
-			$.ajax({
-				url: '../php/consultaUsuarios.php',
-				type: 'POST',
-				dataType: 'json',
-				data: formData,	
-			})
-			.done(function(data) {
-				console.log(data);
-					$('#resultadoConsulta').append("<p>"+data.numero+", " + data.nombre + ", " + data.email + ", " + data.rol + "</p>");
-			})
-			.fail(function() {
-				alert("Fallo");
-				console.log("error");
-			})
-			.always(function() {
-				console.log("complete");
-			});
+		if(tipoConsulta == "usuarios"){
+			if (tipoConsultaUsuario == "todos") {
+			var formData = $(this).serializeArray();
+			var user;
+				$.ajax({
+					url: '../php/consultaUsuarios.php',
+					type: 'POST',
+					dataType: 'json',
+					data: formData,	
+				})
+				.done(function(data) {
+					console.log(data);
+					$.each(data, function(index, value) {
+						user = "<p>"+data[index].numero+", " + data[index].nombre + ", " + data[index].email + ", " + data[index].rol + "</p>";
+						$('#resultadoConsulta').append("<table border='1' width='350'><tr><td>"+user+"</td></tr></table>");
+						console.log(data);
+					});
+				
+
+						//$('#resultadoConsulta').append("<p>"+data.numero+", " + data.nombre + ", " + data.email + ", " + data.rol + "</p>");
+				})
+				.fail(function() {
+					alert("Fallo");
+					console.log("error");
+				})
+				.always(function() {
+					console.log("complete");
+				});
+			}else{
+				var formData = $(this).serializeArray();
+				var user;
+				$.ajax({
+					url: '../php/consultaUsuarios.php',
+					type: 'POST',
+					dataType: 'json',
+					data: formData,	
+				})
+				.done(function(data) {
+					console.log(data);
+						user = "<p>"+data.numero+", " + data.nombre + ", " + data.email + ", " + data.rol + "</p>";
+						$('#resultadoConsulta').html("<table border='1' width='350'><tr><td>"+user+"</td></tr></table>");
+				})
+				.fail(function() {
+					alert("Fallo");
+					console.log("error");
+				})
+				.always(function() {
+					console.log("complete");
+				});
+			}
 		}else{
 			var formData = $(this).serializeArray();
 
